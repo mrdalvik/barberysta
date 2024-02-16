9b6a31a3eabbf15aafcf330ed6ca97e5543d4633
Lev Tregubov <tregubovlev@gmail.com>
2024-02-16 22:55:23 +0200

    Added a helper class for time formatting

---
+++ src/lib/time-formatter.js
@@ -0,0 +1,24 @@
+class TimeFormatter {
+    static secondsToMinutesAndSeconds(seconds) {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        let formattedSting = '';
+        if (minutes > 0) {
+            formattedSting += `${minutes} min.`;
+        }
+
+        if (minutes > 0 && seconds > 0) {
+            formattedSting += ' ';
+        }
+
+        if (remainingSeconds > 0) {
+            formattedSting += `${remainingSeconds} sec.`;
+        }
+
+        return formattedSting;
+    }
+}
+
+module.exports = {
+    TimeFormatter,
+};
